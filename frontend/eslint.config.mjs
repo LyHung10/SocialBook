6dd3ce9058680e43a6e9fa5c09b7a2d6cd18b4d6
LyHung10 <lyhung10nctlop95@gmail.com>
2026-06-07 21:08:10 +0700

    fix: update config eslint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,17 +1,12 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
 import unusedImports from "eslint-plugin-unused-imports";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-// ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  ...nextVitals,
+  ...nextTypescript,
+  prettier,
   {
     plugins: {
       "unused-imports": unusedImports,
