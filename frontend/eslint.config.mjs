bbbfb85743f8759c454c80e640cf451304dee2a8
LyHung10 <lyhung10nctlop95@gmail.com>
2026-06-07 21:17:25 +0700

    fix : update eslint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = [
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-require-imports": "off",
       "unused-imports/no-unused-imports": "error",
       "unused-imports/no-unused-vars": [
         "warn",
