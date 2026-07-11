2e18fd1f1b64e84d439d60f8fbcad8b1514577d3
Quang Dai Thien <daithien2004@gmail.com>
2026-07-11 13:38:23 +0700

    update: UI

--- frontend/src/components/post/CreatePostModal.tsx
+++ frontend/src/components/post/CreatePostModal.tsx
@@ -127,7 +127,6 @@ export default function CreatePostModal() {
                                 }
                               }}
                               placeholder="Chọn sách cho bài viết..."
-                              onlyLibrary
                             />
                           </FormControl>
                           <FormMessage />
